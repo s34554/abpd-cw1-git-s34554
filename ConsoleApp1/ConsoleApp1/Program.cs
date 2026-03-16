1a4f75a3ae120d0e672fe9f86a5ace1310ebc6bd
Szymon Bak <s34554@pjwskt.edu.pl>
2026-03-16 14:55:57 +0100

    Added initial project idea

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+var rand = new Random();
+const string charPool = 
+    "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz23456789!@#$%";
+Console.WriteLine("Password generator. Enter number of chars: ");
+if (int.TryParse(Console.ReadLine(), out int passLength))
+{
+    var sb = new StringBuilder();
+    for (var i = 0; i < passLength; i++)
+    {
+        var index = rand.Next(charPool.Length);
+        sb.Append(charPool[index]);
+    }
+    Console.WriteLine(sb.ToString());
+}
